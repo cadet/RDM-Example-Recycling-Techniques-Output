11121e0530d63e1a9e7a4bb9ce0fe4d577dd9cb8
Katharina Paul <katharina.paul@rwth-aachen.de>
2025-05-30 15:38:17 +0200

    update README.md

---
+++ src/index.py
@@ -0,0 +1,35 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.16.6
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown]
+# (recycling_example)=
+# # Recycling Techniques
+#
+# The design of preparative chromatography poses major challenges in balancing the interdependence of product purity and yield, as well as the efficiency of the column and its pressure drop.
+# To improve difficult separations, it may be necessary to increase the column's separation efficiency by using a longer column or reducing the particle size of the stationary phase.
+# However, using a longer column or a particles can result in an excessively high pressure drop.
+# To overcome this issue, several recycling techniques exist to effectively improve the separation.
+# By connecting the column's outlet to its inlet, the separation can pass through the column multiple times, without the need for a longer column or smaller particles, and hence, with a manageable pressure drop.
+#
+# In the following different recycling concepts are introduced.
+#
+# ```{toctree}
+# :maxdepth: 1
+#
+# clr_process
+# clr_optimization
+# mrssr_process
+# mrssr_optimization
+# ```
